32cf663a57736bf0952c29e0f6e41e2f9285f6af
lapakota <stalkerzone955@gmail.com>
2023-03-26 13:11:45 +0500

    init backend

---
+++ back/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Articlex')
+    .setDescription('The Articlex API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(4200);
+}
+bootstrap();
